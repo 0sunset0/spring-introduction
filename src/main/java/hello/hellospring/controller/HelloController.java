d4f5107777b04507a8d82209196034aac531f42a
0sunset0 <chi6465618@naver.com>
2022-02-25 17:35:29 +0900

    first commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,50 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello"; //html 렌더링
+    }
+
+    @GetMapping("hello-mvc")
+    public String helMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //정적 파일 찾지 말고 그대로 보여주라는 의미
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
